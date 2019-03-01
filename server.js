3fb508ba63cc718bbc2eb3d25e91112e12ca4b3b
kaushik2901 <kaushikjadav602@gmail.com>
2019-03-01 23:16:32 +0530

    Mongo setup

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 2999;
+const MONGODB = process.env.MONGODB || 'mongodb://road123:road123@ds135724.mlab.com:35724/roadgrievancedb';
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect("mongodb://localhost:27017/images", { useNewUrlParser: true });
+mongoose.connect(MONGODB, { useNewUrlParser: true });
 const connection = mongoose.connection;
 mongoose.Promise = global.Promise;
 
